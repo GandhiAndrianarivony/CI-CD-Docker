57034474ab5b96328c4dac22e09ca68c7be02b53
GandhiAndrianarivony <gandhi.andrianarivony@gmail.com>
2023-12-21 15:43:09 +0300

    new commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+"""Blog post model"""
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    """Blog model used to store blog information"""
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+    def __str__(self):
+        return f"{self.title}"
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,24 @@
+"""Module to test blog post behaviour"""
+
 from django.test import TestCase
 
+from .models import Post
+
+
 # Create your tests here.
+class ModelTesting(TestCase):
+    """This class is used to test Post model"""
+
+    def setUp(self):
+        """Setting up all used data for the test"""
+        self.blog = Post.objects.create(
+            title="django-title", author="django-author", slug="django-slug"
+        )
+
+    def test_post_model(self):
+        """Test Post model"""
+        self.assertTrue(isinstance(self.blog, Post))
+        self.assertEqual(str(self.blog), "django-title")
+        # self.assertEqual(self.blog.title, "django-title")
+        # self.assertEqual(self.blog.author, "django-author")
+        # self.assertEqual(self.blog.slug, "django-slug")
